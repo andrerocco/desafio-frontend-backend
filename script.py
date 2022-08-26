6e181c05c677cbc633ce621c7b469b5244ab272d
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-08-26 19:32:24 -0300

    Algorítmo sem otimização traduzido para Python

---
+++ script.py
@@ -0,0 +1,31 @@
+import time
+
+number = 5000
+
+inicio = time.time() # Armazena o instante de inicialização do loop para a solução do problema
+inicioCPU = time.process_time()
+
+# Algorítmo de solução
+resultado = 0
+divisoresAnterior = 0
+for i in range(1, number):
+    divisoresAtual = 0
+
+    for k in range(i, 0, -1):
+        if (i % k == 0): 
+            divisoresAtual += 1
+
+    if (divisoresAtual == divisoresAnterior):
+        resultado += 1
+
+    divisoresAnterior = divisoresAtual
+
+print(resultado)
+
+fim = time.time() # Armazena o instante final em que o loop terminou de ser executado
+fimCPU = time.process_time()
+tempoDecorrido = float(fim - inicio) # Calcula o tempo para a solução
+tempoDecorridoCPU = float(fimCPU - inicioCPU)
+
+print('Tempo: ' + str(tempoDecorrido))
+print('Tempo de CPU: ' + str(tempoDecorridoCPU))
