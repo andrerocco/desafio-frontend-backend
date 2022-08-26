d409d6a916d69f39217fc76bf97b4832dc324b20
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-08-26 18:37:07 -0300

    Criado algorítmo sem otimização

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let number = 1;
+let number = 16;
 
-let start = performance.now(); // Armazena o instante de inicialização do loop para a solução do problema
+let inicio = performance.now(); // Armazena o instante de inicialização do loop para a solução do problema
 
-// Algorítmo de solução
-for (let i = 0; i = number; i++) {
+/* Algorítmo de solução */
+let resultado = 0;
+let divisoresAnterior = 0;
+for (let i = 1; i < number; i++) {
+    let divisoresAtual = 0;
 
+    for (let k = i; k > 0; k--) {
+        if (i % k == 0) { 
+            divisoresAtual++;
+        }
+    };
+
+    if (divisoresAtual == divisoresAnterior) {
+        resultado++;
+    };
+
+    divisoresAnterior = divisoresAtual
 };
 
-let end = performance.now(); // Armazena o instante final em que o loop terminou de ser executado
+console.log(resultado)
 
-// Cálculo do tempo de solução
-let timeElapsed = start - end
+let fim = performance.now(); // Armazena o instante final em que o loop terminou de ser executado
+let tempoDecorrido = fim - inicio // Calcula o tempo para a solução
+
+console.log(tempoDecorrido)
