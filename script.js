8049c38b16b424e19f0b71cb250b4b0a54de0d7f
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-08-26 18:15:36 -0300

    Adicionado monitoramento do tempo decorrido

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = 1;
+
+let start = performance.now(); // Armazena o instante de inicialização do loop para a solução do problema
+
+// Algorítmo de solução
+for (let i = 0; i = number; i++) {
+
+};
+
+let end = performance.now(); // Armazena o instante final em que o loop terminou de ser executado
+
+// Cálculo do tempo de solução
+let timeElapsed = start - end
