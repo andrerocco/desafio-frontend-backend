4d77a12286f6ece22e08fe2069ebca6cee98d373
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-08-26 21:28:34 -0300

    Otimização do algorítmo

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import time
+from math import sqrt
 
-number = 5000
+number = 100000
 
 inicio = time.time() # Armazena o instante de inicialização do loop para a solução do problema
 inicioCPU = time.process_time()
@@ -11,9 +12,12 @@ divisoresAnterior = 0
 for i in range(1, number):
     divisoresAtual = 0
 
-    for k in range(i, 0, -1):
+    limit = int(sqrt(i))
+    for k in range(1, limit+1):
         if (i % k == 0): 
             divisoresAtual += 1
+            if (k != i/k):
+                divisoresAtual += 1
 
     if (divisoresAtual == divisoresAnterior):
         resultado += 1
